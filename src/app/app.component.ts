755b31bab72d031e3c4f159fca3bdc2df73247e0
boghdady <boghdady107@gmail.com>
2022-08-03 01:25:14 +0200

    prepare angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'trello-clone';
-}
+export class AppComponent {}
