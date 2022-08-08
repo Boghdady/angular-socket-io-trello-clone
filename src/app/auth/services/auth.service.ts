ea22b9b71d6f067462f6891f5ad93befe09a3da7
boghdady <boghdady107@gmail.com>
2022-08-08 12:54:55 +0200

    auth guard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, filter, map, Observable } from 'rxjs';
 import { CurrentUserInterface } from '../types/currentUser.interface';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
@@ -8,10 +8,17 @@ import { LoginRequestInterface } from '../types/loginRequest.interface';
 
 @Injectable()
 export class AuthService {
-  // define stream
+  // define stream - we can access this stream from anywhere inside our application
   currentUser$ = new BehaviorSubject<CurrentUserInterface | null | undefined>(
     undefined
   );
+
+  // transform currentUser$ stream to true or false depend on if is null or have user value
+  isLoggedIn$ = this.currentUser$.pipe(
+    filter((currentUser) => currentUser !== undefined),
+    map(Boolean)
+  );
+
   constructor(private http: HttpClient) {}
 
   getCurrentUser(): Observable<CurrentUserInterface> {
