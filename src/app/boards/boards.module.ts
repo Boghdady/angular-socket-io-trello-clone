b21f625e7714da1ae96519a96983704c60b7ff0c
boghdady <boghdady107@gmail.com>
2022-08-17 13:15:41 +0200

    get user board - board service

--- src/app/boards/boards.module.ts
+++ src/app/boards/boards.module.ts
@@ -1,17 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { BoardService } from '../shared/services/board.service';
 import { BoardsComponent } from './components/boards.component';
 
 const routes: Routes = [
   {
     path: 'boards',
     component: BoardsComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [BoardsComponent],
+  providers: [BoardService],
 })
 export class BoardsModule {}
--- src/app/boards/components/boards.component.ts
+++ src/app/boards/components/boards.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BoardService } from 'src/app/shared/services/board.service';
 
 @Component({
   selector: 'boards',
   templateUrl: './boards.component.html',
 })
-export class BoardsComponent {}
+export class BoardsComponent implements OnInit {
+  constructor(private boardService: BoardService) {}
+
+  ngOnInit(): void {
+    this.boardService.getBoards().subscribe((boards) => {
+      console.log('result: ', boards);
+    });
+  }
+}
